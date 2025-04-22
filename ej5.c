9981f5820332277cbf10015f672c598bc2366d56
micacorvera <123522140+micacorvera@users.noreply.github.com>
2025-04-22 17:06:57 -0300

    Add files via upload

--- ej5.c
+++ ej5.c
@@ -7,26 +7,25 @@ int main() {
     int i, N, num, suma3, cant=0, pares=0;
     printf("Ingrese un numero natural: \n");
     scanf("%d", &N);
+    if (N>0)
+   {
     printf("Ingrese %d numeros naturales \n", N);
-    if (N>0){
-        printf("Ingrese %d numeros naturales \n", N);
-        for(i=0;i<N;i++) {
-            scanf("%d", &num);
-            if(num%3==0) {
-                suma3+=num;
-            }
-            if(num%5==0) {
-                cant++;
-            }
-            if(i%2==0) {
-                pares+=num;
-            }
+    for(i=0;i<N;i++) {
+        scanf("%d", &num);
+        if(num%3==0) {
+            suma3+=num;
         }
+        if(num%5==0) {
+            cant++;
+        }
+        if(i%2==0) {
+            pares+=num;
+        }
+    }
         }
         else{
-             printf("El valor ingresado no es un numero natural");
-        }
-        
+        printf("El valor ingresado no es un numero natural");
+    }
     printf("sumatoria de los numeros multiplos de 3: %d \n", suma3);
     printf("cantidad de multiplos de 5: %d \n", cant);
     printf("Numeros ingresados en orden par: %d \n", pares);
